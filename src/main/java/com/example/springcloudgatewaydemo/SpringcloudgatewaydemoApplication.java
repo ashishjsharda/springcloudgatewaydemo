1a6e5dd5b00a8430bbc45a5ddc40b3df4acce81c
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-02 08:18:32 -0600

    first commit

---
+++ src/main/java/com/example/springcloudgatewaydemo/SpringcloudgatewaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudgatewaydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudgatewaydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudgatewaydemo/SpringcloudgatewaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudgatewaydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
